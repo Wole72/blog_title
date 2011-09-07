3f0848cf89f1aa22d430df73616737b45f34d2e2
Oluwawole <oluwawole@oluwawole-VirtualBox.(none)>
2011-09-07 18:13:36 -0400

    Created new blog title

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,20 @@
 #
 # Create your "Blog" class in this file.
 #
+
+class Blog
+  def initialize url = nil
+    @url = url
+    #@latest_title = nil
+  end
+  
+  def latest_title
+   course_curriculum = Net::HTTP.get(URI.parse(@url))
+
+    doc = open(course_curriculum)
+    (doc/"/html/body/div[2]/div/div[2]/div/div/h3").inner_html
+  end
+end
+
+
+
